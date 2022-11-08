f62a3e11f8326c6dfe43127727fc5cef0cf6205b
Björn Löndahl <bjorn.londahl@dugga.com>
2022-11-08 13:41:22 +0100

    First commit

---
+++ MapleGriffin.CurrencyCodes/CountryCode.cs
@@ -0,0 +1,496 @@
+﻿namespace MapleGriffin.CurrencyCodes
+{
+    public enum CountryCode
+    {
+        ///<summary>Afghanistan</summary>
+		AF=4,
+        ///<summary>Albania</summary>
+		AL=8,
+        ///<summary>Algeria</summary>
+		DZ=12,
+        ///<summary>American Samoa</summary>
+		AS=16,
+        ///<summary>Andorra</summary>
+		AD=20,
+        ///<summary>Angola</summary>
+		AO=24,
+        ///<summary>Anguilla</summary>
+		AI=660,
+        ///<summary>Antigua and Barbuda</summary>
+		AG=28,
+        ///<summary>Argentina</summary>
+		AR=32,
+        ///<summary>Armenia</summary>
+		AM=51,
+        ///<summary>Aruba</summary>
+		AW=533,
+        ///<summary>Australia</summary>
+		AU=36,
+        ///<summary>Austria</summary>
+		AT=40,
+        ///<summary>Azerbaijan</summary>
+		AZ=31,
+        ///<summary>The Bahamas</summary>
+		BS=44,
+        ///<summary>Bahrain</summary>
+		BH=48,
+        ///<summary>Bangladesh</summary>
+		BD=50,
+        ///<summary>Barbados</summary>
+		BB=52,
+        ///<summary>Belarus</summary>
+		BY=112,
+        ///<summary>Belgium</summary>
+		BE=56,
+        ///<summary>Belize</summary>
+		BZ=84,
+        ///<summary>Benin</summary>
+		BJ=204,
+        ///<summary>Bermuda</summary>
+		BM=60,
+        ///<summary>Bhutan</summary>
+		BT=64,
+        ///<summary>Bolivia</summary>
+		BO=68,
+        ///<summary>Bonaire</summary>
+		BQ=535,
+        ///<summary>Bosnia and Herzegovina</summary>
+		BA=70,
+        ///<summary>Botswana</summary>
+		BW=72,
+        ///<summary>Bouvet Island</summary>
+		BV=74,
+        ///<summary>Brazil</summary>
+		BR=76,
+        ///<summary>British Indian Ocean Territory</summary>
+		IO=86,
+        ///<summary>Brunei</summary>
+		BN=96,
+        ///<summary>Bulgaria</summary>
+		BG=100,
+        ///<summary>Burkina Faso</summary>
+		BF=854,
+        ///<summary>Burundi</summary>
+		BI=108,
+        ///<summary>Cape Verde</summary>
+		CV=132,
+        ///<summary>Cambodia</summary>
+		KH=116,
+        ///<summary>Cameroon</summary>
+		CM=120,
+        ///<summary>Canada</summary>
+		CA=124,
+        ///<summary>Cayman Islands</summary>
+		KY=136,
+        ///<summary>Central African Republic</summary>
+		CF=140,
+        ///<summary>Chad</summary>
+		TD=148,
+        ///<summary>Chile</summary>
+		CL=152,
+        ///<summary>China</summary>
+		CN=156,
+        ///<summary>Christmas Island</summary>
+		CX=162,
+        ///<summary>Cocos (Keeling) Islands</summary>
+		CC=166,
+        ///<summary>Colombia</summary>
+		CO=170,
+        ///<summary>Comoros</summary>
+		KM=174,
+        ///<summary>Democratic Republic of the Congo</summary>
+		CD=180,
+        ///<summary>Republic of the Congo</summary>
+		CG=178,
+        ///<summary>Cook Islands</summary>
+		CK=184,
+        ///<summary>Costa Rica</summary>
+		CR=188,
+        ///<summary>Croatia</summary>
+		HR=191,
+        ///<summary>Cuba</summary>
+		CU=192,
+        ///<summary>Curaçao</summary>
+		CW=531,
+        ///<summary>Cyprus</summary>
+		CY=196,
+        ///<summary>Czech Republic</summary>
+		CZ=203,
+        ///<summary>Ivory Coast</summary>
+		CI=384,
+        ///<summary>Denmark</summary>
+		DK=208,
+        ///<summary>Djibouti</summary>
+		DJ=262,
+        ///<summary>Dominica</summary>
+		DM=212,
+        ///<summary>Dominican Republic</summary>
+		DO=214,
+        ///<summary>Ecuador</summary>
+		EC=218,
+        ///<summary>Egypt</summary>
+		EG=818,
+        ///<summary>El Salvador</summary>
+		SV=222,
+        ///<summary>Equatorial Guinea</summary>
+		GQ=226,
+        ///<summary>Eritrea</summary>
+		ER=232,
+        ///<summary>Estonia</summary>
+		EE=233,
+        ///<summary>Ethiopia</summary>
+		ET=231,
+        ///<summary>Falkland Islands</summary>
+		FK=238,
+        ///<summary>Faroe Islands</summary>
+		FO=234,
+        ///<summary>Fiji</summary>
+		FJ=242,
+        ///<summary>Finland</summary>
+		FI=246,
+        ///<summary>France</summary>
+		FR=250,
+        ///<summary>French Guiana</summary>
+		GF=254,
+        ///<summary>French Polynesia</summary>
+		PF=258,
+        ///<summary>French Southern and Antarctic Lands</summary>
+		TF=260,
+        ///<summary>Gabon</summary>
+		GA=266,
+        ///<summary>The Gambia</summary>
+		GM=270,
+        ///<summary>Georgia</summary>
+		GE=268,
+        ///<summary>Germany</summary>
+		DE=276,
+        ///<summary>Ghana</summary>
+		GH=288,
+        ///<summary>Gibraltar</summary>
+		GI=292,
+        ///<summary>Greece</summary>
+		GR=300,
+        ///<summary>Greenland</summary>
+		GL=304,
+        ///<summary>Grenada</summary>
+		GD=308,
+        ///<summary>Guadeloupe</summary>
+		GP=312,
+        ///<summary>Guam</summary>
+		GU=316,
+        ///<summary>Guatemala</summary>
+		GT=320,
+        ///<summary>Bailiwick of Guernsey</summary>
+		GG=831,
+        ///<summary>Guinea</summary>
+		GN=324,
+        ///<summary>Guinea-Bissau</summary>
+		GW=624,
+        ///<summary>Guyana</summary>
+		GY=328,
+        ///<summary>Haiti</summary>
+		HT=332,
+        ///<summary>Heard Island and McDonald Islands</summary>
+		HM=334,
+        ///<summary>Holy See</summary>
+		VA=336,
+        ///<summary>Honduras</summary>
+		HN=340,
+        ///<summary>Hong Kong</summary>
+		HK=344,
+        ///<summary>Hungary</summary>
+		HU=348,
+        ///<summary>Iceland</summary>
+		IS=352,
+        ///<summary>India</summary>
+		IN=356,
+        ///<summary>Indonesia</summary>
+		ID=360,
+        ///<summary>Iran</summary>
+		IR=364,
+        ///<summary>Iraq</summary>
+		IQ=368,
+        ///<summary>Republic of Ireland</summary>
+		IE=372,
+        ///<summary>Isle of Man</summary>
+		IM=833,
+        ///<summary>Israel</summary>
+		IL=376,
+        ///<summary>Italy</summary>
+		IT=380,
+        ///<summary>Jamaica</summary>
+		JM=388,
+        ///<summary>Japan</summary>
+		JP=392,
+        ///<summary>Jersey</summary>
+		JE=832,
+        ///<summary>Jordan</summary>
+		JO=400,
+        ///<summary>Kazakhstan</summary>
+		KZ=398,
+        ///<summary>Kenya</summary>
+		KE=404,
+        ///<summary>Kiribati</summary>
+		KI=296,
+        ///<summary>North Korea</summary>
+		KP=408,
+        ///<summary>South Korea</summary>
+		KR=410,
+        ///<summary>Kuwait</summary>
+		KW=414,
+        ///<summary>Kyrgyzstan</summary>
+		KG=417,
+        ///<summary>Laos</summary>
+		LA=418,
+        ///<summary>Latvia</summary>
+		LV=428,
+        ///<summary>Lebanon</summary>
+		LB=422,
+        ///<summary>Lesotho</summary>
+		LS=426,
+        ///<summary>Liberia</summary>
+		LR=430,
+        ///<summary>Libya</summary>
+		LY=434,
+        ///<summary>Liechtenstein</summary>
+		LI=438,
+        ///<summary>Lithuania</summary>
+		LT=440,
+        ///<summary>Luxembourg</summary>
+		LU=442,
+        ///<summary>Macau</summary>
+		MO=446,
+        ///<summary>Madagascar</summary>
+		MG=450,
+        ///<summary>Malawi</summary>
+		MW=454,
+        ///<summary>Malaysia</summary>
+		MY=458,
+        ///<summary>Maldives</summary>
+		MV=462,
+        ///<summary>Mali</summary>
+		ML=466,
+        ///<summary>Malta</summary>
+		MT=470,
+        ///<summary>Marshall Islands</summary>
+		MH=584,
+        ///<summary>Martinique</summary>
+		MQ=474,
+        ///<summary>Mauritania</summary>
+		MR=478,
+        ///<summary>Mauritius</summary>
+		MU=480,
+        ///<summary>Mayotte</summary>
+		YT=175,
+        ///<summary>Mexico</summary>
+		MX=484,
+        ///<summary>Federated States of Micronesia</summary>
+		FM=583,
+        ///<summary>Moldova</summary>
+		MD=498,
+        ///<summary>Monaco</summary>
+		MC=492,
+        ///<summary>Mongolia</summary>
+		MN=496,
+        ///<summary>Montenegro</summary>
+		ME=499,
+        ///<summary>Montserrat</summary>
+		MS=500,
+        ///<summary>Morocco</summary>
+		MA=504,
+        ///<summary>Mozambique</summary>
+		MZ=508,
+        ///<summary>Myanmar</summary>
+		MM=104,
+        ///<summary>Namibia</summary>
+		NA=516,
+        ///<summary>Nauru</summary>
+		NR=520,
+        ///<summary>Nepal</summary>
+		NP=524,
+        ///<summary>Netherlands</summary>
+		NL=528,
+        ///<summary>New Caledonia</summary>
+		NC=540,
+        ///<summary>New Zealand</summary>
+		NZ=554,
+        ///<summary>Nicaragua</summary>
+		NI=558,
+        ///<summary>Niger</summary>
+		NE=562,
+        ///<summary>Nigeria</summary>
+		NG=566,
+        ///<summary>Niue</summary>
+		NU=570,
+        ///<summary>Norfolk Island</summary>
+		NF=574,
+        ///<summary>Northern Mariana Islands</summary>
+		MP=580,
+        ///<summary>Norway</summary>
+		NO=578,
+        ///<summary>Oman</summary>
+		OM=512,
+        ///<summary>Pakistan</summary>
+		PK=586,
+        ///<summary>Palau</summary>
+		PW=585,
+        ///<summary>Panama</summary>
+		PA=591,
+        ///<summary>Papua New Guinea</summary>
+		PG=598,
+        ///<summary>Paraguay</summary>
+		PY=600,
+        ///<summary>Peru</summary>
+		PE=604,
+        ///<summary>Philippines</summary>
+		PH=608,
+        ///<summary>Pitcairn Islands</summary>
+		PN=612,
+        ///<summary>Poland</summary>
+		PL=616,
+        ///<summary>Portugal</summary>
+		PT=620,
+        ///<summary>Puerto Rico</summary>
+		PR=630,
+        ///<summary>Qatar</summary>
+		QA=634,
+        ///<summary>North Macedonia</summary>
+		MK=807,
+        ///<summary>Romania</summary>
+		RO=642,
+        ///<summary>Russia</summary>
+		RU=643,
+        ///<summary>Rwanda</summary>
+		RW=646,
+        ///<summary>Réunion</summary>
+		RE=638,
+        ///<summary>Saint Barthélemy</summary>
+		BL=652,
+        ///<summary>Saint Helena</summary>
+		SH=654,
+        ///<summary>Saint Kitts and Nevis</summary>
+		KN=659,
+        ///<summary>Saint Lucia</summary>
+		LC=662,
+        ///<summary>Collectivity of Saint Martin</summary>
+		MF=663,
+        ///<summary>Saint Pierre and Miquelon</summary>
+		PM=666,
+        ///<summary>Saint Vincent and the Grenadines</summary>
+		VC=670,
+        ///<summary>Samoa</summary>
+		WS=882,
+        ///<summary>San Marino</summary>
+		SM=674,
+        ///<summary>São Tomé and Príncipe</summary>
+		ST=678,
+        ///<summary>Saudi Arabia</summary>
+		SA=682,
+        ///<summary>Senegal</summary>
+		SN=686,
+        ///<summary>Serbia</summary>
+		RS=688,
+        ///<summary>Seychelles</summary>
+		SC=690,
+        ///<summary>Sierra Leone</summary>
+		SL=694,
+        ///<summary>Singapore</summary>
+		SG=702,
+        ///<summary>Sint Maarten</summary>
+		SX=534,
+        ///<summary>Slovakia</summary>
+		SK=703,
+        ///<summary>Slovenia</summary>
+		SI=705,
+        ///<summary>Solomon Islands</summary>
+		SB=90,
+        ///<summary>Somalia</summary>
+		SO=706,
+        ///<summary>South Africa</summary>
+		ZA=710,
+        ///<summary>South Sudan</summary>
+		SS=728,
+        ///<summary>Spain</summary>
+		ES=724,
+        ///<summary>Sri Lanka</summary>
+		LK=144,
+        ///<summary>Sudan</summary>
+		SD=729,
+        ///<summary>Suriname</summary>
+		SR=740,
+        ///<summary>Svalbard</summary>
+		SJ=744,
+        ///<summary>Eswatini</summary>
+		SZ=748,
+        ///<summary>Sweden</summary>
+		SE=752,
+        ///<summary>Switzerland</summary>
+		CH=756,
+        ///<summary>Syria</summary>
+		SY=760,
+        ///<summary>Taiwan</summary>
+		TW=158,
+        ///<summary>Tajikistan</summary>
+		TJ=762,
+        ///<summary>Tanzania</summary>
+		TZ=834,
+        ///<summary>Thailand</summary>
+		TH=764,
+        ///<summary>East Timor</summary>
+		TL=626,
+        ///<summary>Togo</summary>
+		TG=768,
+        ///<summary>Tokelau</summary>
+		TK=772,
+        ///<summary>Tonga</summary>
+		TO=776,
+        ///<summary>Trinidad and Tobago</summary>
+		TT=780,
+        ///<summary>Tunisia</summary>
+		TN=788,
+        ///<summary>Turkey</summary>
+		TR=792,
+        ///<summary>Turkmenistan</summary>
+		TM=795,
+        ///<summary>Turks and Caicos Islands</summary>
+		TC=796,
+        ///<summary>Tuvalu</summary>
+		TV=798,
+        ///<summary>Uganda</summary>
+		UG=800,
+        ///<summary>Ukraine</summary>
+		UA=804,
+        ///<summary>United Arab Emirates</summary>
+		AE=784,
+        ///<summary>United Kingdom</summary>
+		GB=826,
+        ///<summary>United States</summary>
+		US=840,
+        ///<summary>Uruguay</summary>
+		UY=858,
+        ///<summary>Uzbekistan</summary>
+		UZ=860,
+        ///<summary>Vanuatu</summary>
+		VU=548,
+        ///<summary>Venezuela</summary>
+		VE=862,
+        ///<summary>Vietnam</summary>
+		VN=704,
+        ///<summary>British Virgin Islands</summary>
+		VG=92,
+        ///<summary>United States Virgin Islands</summary>
+		VI=850,
+        ///<summary>Wallis and Futuna</summary>
+		WF=876,
+        ///<summary>Western Sahara</summary>
+		EH=732,
+        ///<summary>Yemen</summary>
+		YE=887,
+        ///<summary>Zambia</summary>
+		ZM=894,
+        ///<summary>Zimbabwe</summary>
+		ZW=716,
+        ///<summary>Åland</summary>
+		AX=248
+    }
+}
---
+++ MapleGriffin.CurrencyCodes/CurrencyCode.cs
@@ -0,0 +1,320 @@
+﻿namespace MapleGriffin.CurrencyCodes
+{
+    public enum CurrencyCode
+    {
+        ///<summary>Lek</summary>
+		ALL=8,
+        ///<summary>Algerian Dinar</summary>
+		DZD=12,
+        ///<summary>Argentine Peso</summary>
+		ARS=32,
+        ///<summary>Australian Dollar</summary>
+		AUD=36,
+        ///<summary>Bahamian Dollar</summary>
+		BSD=44,
+        ///<summary>Bahraini Dinar</summary>
+		BHD=48,
+        ///<summary>Taka</summary>
+		BDT=50,
+        ///<summary>Armenian Dram</summary>
+		AMD=51,
+        ///<summary>Barbados Dollar</summary>
+		BBD=52,
+        ///<summary>Bermudian Dollar</summary>
+		BMD=60,
+        ///<summary>Ngultrum</summary>
+		BTN=64,
+        ///<summary>Boliviano</summary>
+		BOB=68,
+        ///<summary>Pula</summary>
+		BWP=72,
+        ///<summary>Belize Dollar</summary>
+		BZD=84,
+        ///<summary>Solomon Islands Dollar</summary>
+		SBD=90,
+        ///<summary>Brunei Dollar</summary>
+		BND=96,
+        ///<summary>Kyat</summary>
+		MMK=104,
+        ///<summary>Burundi Franc</summary>
+		BIF=108,
+        ///<summary>Riel</summary>
+		KHR=116,
+        ///<summary>Canadian Dollar</summary>
+		CAD=124,
+        ///<summary>Cabo Verde Escudo</summary>
+		CVE=132,
+        ///<summary>Cayman Islands Dollar</summary>
+		KYD=136,
+        ///<summary>Sri Lanka Rupee</summary>
+		LKR=144,
+        ///<summary>Chilean Peso</summary>
+		CLP=152,
+        ///<summary>Yuan Renminbi</summary>
+		CNY=156,
+        ///<summary>Colombian Peso</summary>
+		COP=170,
+        ///<summary>Comoro Franc</summary>
+		KMF=174,
+        ///<summary>Costa Rican Colon</summary>
+		CRC=188,
+        ///<summary>Kuna</summary>
+		HRK=191,
+        ///<summary>Cuban Peso</summary>
+		CUP=192,
+        ///<summary>Czech Koruna</summary>
+		CZK=203,
+        ///<summary>Danish Krone</summary>
+		DKK=208,
+        ///<summary>Dominican Peso</summary>
+		DOP=214,
+        ///<summary>El Salvador Colon</summary>
+		SVC=222,
+        ///<summary>Ethiopian Birr</summary>
+		ETB=230,
+        ///<summary>Nakfa</summary>
+		ERN=232,
+        ///<summary>Falkland Islands Pound</summary>
+		FKP=238,
+        ///<summary>Fiji Dollar</summary>
+		FJD=242,
+        ///<summary>Djibouti Franc</summary>
+		DJF=262,
+        ///<summary>Dalasi</summary>
+		GMD=270,
+        ///<summary>Gibraltar Pound</summary>
+		GIP=292,
+        ///<summary>Quetzal</summary>
+		GTQ=320,
+        ///<summary>Guinea Franc</summary>
+		GNF=324,
+        ///<summary>Guyana Dollar</summary>
+		GYD=328,
+        ///<summary>Gourde</summary>
+		HTG=332,
+        ///<summary>Lempira</summary>
+		HNL=340,
+        ///<summary>Hong Kong Dollar</summary>
+		HKD=344,
+        ///<summary>Forint</summary>
+		HUF=348,
+        ///<summary>Iceland Krona</summary>
+		ISK=352,
+        ///<summary>Indian Rupee</summary>
+		INR=356,
+        ///<summary>Rupiah</summary>
+		IDR=360,
+        ///<summary>Iranian Rial</summary>
+		IRR=364,
+        ///<summary>Iraqi Dinar</summary>
+		IQD=368,
+        ///<summary>New Israeli Sheqel</summary>
+		ILS=376,
+        ///<summary>Jamaican Dollar</summary>
+		JMD=388,
+        ///<summary>Yen</summary>
+		JPY=392,
+        ///<summary>Tenge</summary>
+		KZT=398,
+        ///<summary>Jordanian Dinar</summary>
+		JOD=400,
+        ///<summary>Kenyan Shilling</summary>
+		KES=404,
+        ///<summary>North Korean Won</summary>
+		KPW=408,
+        ///<summary>Won</summary>
+		KRW=410,
+        ///<summary>Kuwaiti Dinar</summary>
+		KWD=414,
+        ///<summary>Som</summary>
+		KGS=417,
+        ///<summary>Kip</summary>
+		LAK=418,
+        ///<summary>Lebanese Pound</summary>
+		LBP=422,
+        ///<summary>Loti</summary>
+		LSL=426,
+        ///<summary>Liberian Dollar</summary>
+		LRD=430,
+        ///<summary>Libyan Dinar</summary>
+		LYD=434,
+        ///<summary>Pataca</summary>
+		MOP=446,
+        ///<summary>Kwacha</summary>
+		MWK=454,
+        ///<summary>Malaysian Ringgit</summary>
+		MYR=458,
+        ///<summary>Rufiyaa</summary>
+		MVR=462,
+        ///<summary>Mauritius Rupee</summary>
+		MUR=480,
+        ///<summary>Mexican Peso</summary>
+		MXN=484,
+        ///<summary>Tugrik</summary>
+		MNT=496,
+        ///<summary>Moldovan Leu</summary>
+		MDL=498,
+        ///<summary>Moroccan Dirham</summary>
+		MAD=504,
+        ///<summary>Rial Omani</summary>
+		OMR=512,
+        ///<summary>Namibia Dollar</summary>
+		NAD=516,
+        ///<summary>Nepalese Rupee</summary>
+		NPR=524,
+        ///<summary>Netherlands Antillean Guilder</summary>
+		ANG=532,
+        ///<summary>Aruban Florin</summary>
+		AWG=533,
+        ///<summary>Vatu</summary>
+		VUV=548,
+        ///<summary>New Zealand Dollar</summary>
+		NZD=554,
+        ///<summary>Cordoba Oro</summary>
+		NIO=558,
+        ///<summary>Naira</summary>
+		NGN=566,
+        ///<summary>Norwegian Krone</summary>
+		NOK=578,
+        ///<summary>Pakistan Rupee</summary>
+		PKR=586,
+        ///<summary>Balboa</summary>
+		PAB=590,
+        ///<summary>Kina</summary>
+		PGK=598,
+        ///<summary>Guarani</summary>
+		PYG=600,
+        ///<summary>Nuevo Sol</summary>
+		PEN=604,
+        ///<summary>Philippine Peso</summary>
+		PHP=608,
+        ///<summary>Qatari Rial</summary>
+		QAR=634,
+        ///<summary>Russian Ruble</summary>
+		RUB=643,
+        ///<summary>Rwanda Franc</summary>
+		RWF=646,
+        ///<summary>Saint Helena Pound</summary>
+		SHP=654,
+        ///<summary>Saudi Riyal</summary>
+		SAR=682,
+        ///<summary>Seychelles Rupee</summary>
+		SCR=690,
+        ///<summary>Singapore Dollar</summary>
+		SGD=702,
+        ///<summary>Dong</summary>
+		VND=704,
+        ///<summary>Somali Shilling</summary>
+		SOS=706,
+        ///<summary>Rand</summary>
+		ZAR=710,
+        ///<summary>South Sudanese Pound</summary>
+		SSP=728,
+        ///<summary>Lilangeni</summary>
+		SZL=748,
+        ///<summary>Swedish Krona</summary>
+		SEK=752,
+        ///<summary>Swiss Franc</summary>
+		CHF=756,
+        ///<summary>Syrian Pound</summary>
+		SYP=760,
+        ///<summary>Baht</summary>
+		THB=764,
+        ///<summary>Pa’anga</summary>
+		TOP=776,
+        ///<summary>Trinidad and Tobago Dollar</summary>
+		TTD=780,
+        ///<summary>UAE Dirham</summary>
+		AED=784,
+        ///<summary>Tunisian Dinar</summary>
+		TND=788,
+        ///<summary>Uganda Shilling</summary>
+		UGX=800,
+        ///<summary>Denar</summary>
+		MKD=807,
+        ///<summary>Egyptian Pound</summary>
+		EGP=818,
+        ///<summary>Pound Sterling</summary>
+		GBP=826,
+        ///<summary>Tanzanian Shilling</summary>
+		TZS=834,
+        ///<summary>US Dollar</summary>
+		USD=840,
+        ///<summary>Peso Uruguayo</summary>
+		UYU=858,
+        ///<summary>Uzbekistan Sum</summary>
+		UZS=860,
+        ///<summary>Tala</summary>
+		WST=882,
+        ///<summary>Yemeni Rial</summary>
+		YER=886,
+        ///<summary>New Taiwan Dollar</summary>
+		TWD=901,
+        ///<summary>Leone</summary>
+		SLE=925,
+        ///<summary>Bolivar</summary>
+		VES=928,
+        ///<summary>Ouguiya</summary>
+		MRU=929,
+        ///<summary>Dobra</summary>
+		STN=930,
+        ///<summary>Zimbabwe Dollar</summary>
+		ZWL=932,
+        ///<summary>Belarussian Ruble</summary>
+		BYN=933,
+        ///<summary>Turkmenistan New Manat</summary>
+		TMT=934,
+        ///<summary>Ghana Cedi</summary>
+		GHS=936,
+        ///<summary>Sudanese Pound</summary>
+		SDG=938,
+        ///<summary>Uruguay Peso</summary>
+		UYI=940,
+        ///<summary>Serbian Dinar</summary>
+		RSD=941,
+        ///<summary>Mozambique Metical</summary>
+		MZN=943,
+        ///<summary>Azerbaijanian Manat</summary>
+		AZN=944,
+        ///<summary>Romanian Leu</summary>
+		RON=946,
+        ///<summary>Turkish Lira</summary>
+		TRY=949,
+        ///<summary>Central African CFA franc</summary>
+		XAF=950,
+        ///<summary>East Caribbean Dollar</summary>
+		XCD=951,
+        ///<summary>West African CFA franc</summary>
+		XOF=952,
+        ///<summary>CFP Franc</summary>
+		XPF=953,
+        ///<summary>Zambian Kwacha</summary>
+		ZMW=967,
+        ///<summary>Surinam Dollar</summary>
+		SRD=968,
+        ///<summary>Malagasy Ariary</summary>
+		MGA=969,
+        ///<summary>Afghani</summary>
+		AFN=971,
+        ///<summary>Somoni</summary>
+		TJS=972,
+        ///<summary>Kwanza</summary>
+		AOA=973,
+        ///<summary>Bulgarian Lev</summary>
+		BGN=975,
+        ///<summary>Congolese Franc</summary>
+		CDF=976,
+        ///<summary>Convertible Mark</summary>
+		BAM=977,
+        ///<summary>Euro</summary>
+		EUR=978,
+        ///<summary>Hryvnia</summary>
+		UAH=980,
+        ///<summary>Lari</summary>
+		GEL=981,
+        ///<summary>Zloty</summary>
+		PLN=985,
+        ///<summary>Brazilian Real</summary>
+		BRL=986
+    }
+}
